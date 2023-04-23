297e1d27b7cc558a2ae7a3ec110f07c516b1981f
Fridi-Engel <frdi4808@gmal.com>
2023-04-23 21:45:54 +0300

    first

---
+++ src/service.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:5294/items';
+
+const instance = axios.create();
+
+export default {
+  getTasks: async () => {
+    const result = await instance.get();
+    return result.data;
+  },
+
+  addTask: async (name) => {
+    return await instance.post('',{ Name: name, IsComplete: false })
+  },
+
+  setCompleted: async (id, name, isComplete) => {
+    console.log('setCompleted', { id, name, isComplete })
+    return await instance.put(`/${id}`, { Name: name, IsComplete: isComplete })
+  },
+
+  deleteTask: async (id) => {
+    console.log('deleteTask')
+    return await instance.delete(`/${id}`)
+  }
+};
+
+instance.interceptors.response.use(
+  response => response,
+  error => {
+    console.log("error", error)
+  });
